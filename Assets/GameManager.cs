87209fcd542f34b46ed49daeec8f91a40ec08919
mathcdu <mathieucdufour@gmail.com>
2024-11-05 12:06:05 -0500

    Switch to XR + holePosition
    All set

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private int numeroTrouActuel = 0;
+    public List<Transform> positionDebut;
+    
+    public Rigidbody ballRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballRigidbody.transform.position = positionDebut[numeroTrouActuel].position;
+        ballRigidbody.velocity =  Vector3.zero;
+        ballRigidbody.angularVelocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ProchainTrou(){
+        numeroTrouActuel = numeroTrouActuel +1;
+
+        if(numeroTrouActuel >= positionDebut.Count)
+        {
+            Debug.Log("Fin");
+        }
+        else{
+             ballRigidbody.transform.position = positionDebut[numeroTrouActuel].position;
+             ballRigidbody.velocity =  Vector3.zero;
+             ballRigidbody.angularVelocity = Vector3.zero;
+        }
     }
 }
