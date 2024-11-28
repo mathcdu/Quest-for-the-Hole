1d99179fa7909cb4c492af60dda7aa81cd36eef8
Emily Joly <e1640942@cmaisonneuve.qc.ca>
2024-11-28 13:13:10 -0500

    tuto obstacles
    reste a trouver asset pour speed boost

---
+++ Assets/readme.cs
@@ -0,0 +1,8 @@
+// to do: 
+// colliders sur le parcours de golf + backrooms
+// + ajuster colliders des bosses du parcours
+// menu start (instructions) et pause
+// crédits
+// trouver asset pour speed boost (jump pad de coté??)
+// quand ca téléporte la balle, te téléporter aussi
+// musique + sons
