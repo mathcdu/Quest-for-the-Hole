fd2d6aecdf293603df8dd6048a293bd64cf768e4
Emily Joly <e1640942@cmaisonneuve.qc.ca>
2024-12-12 17:41:03 -0500

    modifs assets + sons

--- Assets/Scripts/PortalScript.cs
+++ Assets/Scripts/PortalScript.cs
@@ -6,16 +6,25 @@ public class PortalScript : MonoBehaviour
 {
     public Transform ball, destination;
     public GameObject ballg;
+    public AudioClip teleportSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Create AudioSource once
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ball"))
         {
             ballg.SetActive(false);
-
             ball.position = destination.position;
-
             ballg.SetActive(true);
+
+            // Play sound
+            audioSource.PlayOneShot(teleportSound);
         }
     }
 }
